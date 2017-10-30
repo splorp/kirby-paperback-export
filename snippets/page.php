3b59ce0792886b4aa61378b094d61347cf490c5f
Grant Hutchinson <grant@splorp.com>
2017-10-30 15:04:10 -0600

    Finoosed the plaintext page loop output

--- snippets/page.php
+++ snippets/page.php
@@ -1,11 +1,9 @@
-@@TOC <?= html($page->title()) ?>
-
-<?= html($page->text()) ?>
-
+<?php echo '@@TOC ' . $page->title() . PHP_EOL . PHP_EOL ?>
+<?php echo html_entity_decode(strip_tags($page->text()->kirbytext())) . PHP_EOL . PHP_EOL ?>
 <?php if($page->content()->has('Source')): ?>
-<?php if($page->source()->pages()->count() > 1) { echo 'Sources'; } else { echo 'Source'; } ?>
+<?php if($page->source()->pages()->count() > 1) { echo 'Sources: '; } else { echo 'Source: '; } ?>
 <? $n=0; foreach($page->source()->pages() as $source): $n++; ?>
-<?php echo html($source->title()) ?>
+<?php echo $source->title() . PHP_EOL ?>
 <?php endforeach ?>
+<?php echo PHP_EOL ?>
 <?php endif ?>
-
