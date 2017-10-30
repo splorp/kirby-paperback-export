367101aec11e6207796f23cd5811dac2ee273581
Grant Hutchinson <grant@splorp.com>
2017-10-30 16:07:38 -0600

    Added Content-Disposition header to force automatic downloads

--- paperback-export.txt.php
+++ paperback-export.txt.php
@@ -1,4 +1,5 @@
 <?php header('Content-Type:text/plain'); ?>
+<?php header('Content-Disposition: attachment; filename="paperback.txt"'); ?>
 <?php echo $title . PHP_EOL . PHP_EOL . $description . PHP_EOL . PHP_EOL ?>
 <?php foreach ($pages as $page) : ?>
 <?php snippet('paperback.page', compact('languages', 'page')) ?>
