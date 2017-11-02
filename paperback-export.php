93c926f834cb35113e544161d438a84cdb08427a
Grant Hutchinson <grant@splorp.com>
2017-11-02 12:55:57 -0600

    Refactored and simplified filtering options

--- paperback-export.php
+++ paperback-export.php
@@ -3,7 +3,7 @@
 /**
  * Kirby Paperback Export
  *
- * @version   1.0.0
+ * @version   1.0.1
  * @author    Grant Hutchinson <grant@splorp.com>
  * @copyright Grant Hutchinson <grant@splorp.com>
  * @link      https://github.com/splorp/kirby-paperback-export
@@ -16,19 +16,15 @@ kirby()->set('route', [
     'pattern' => 'export/paperback',
     'action'  => function() {
 
-        $includePages      = c::get('paperback.include.pages', ['terms']);
-        $includeChildren   = c::get('paperback.include.children', []);
-        $includeTemplates  = c::get('paperback.include.templates', []);
-        $includeInvisibles = c::get('paperback.include.invisible', true);
+        $includeChildren  = c::get('paperback.include.children', ['terms']);
+        $excludeTemplate  = c::get('paperback.exclude.template', []);
+        $includeInvisible = c::get('paperback.include.invisible', true);
 
-        if (! is_array($includePages)) {
-            throw new Exception('The option "paperback.include.pages" must be an array.');
-        }
         if (! is_array($includeChildren)) {
             throw new Exception('The option "paperback.include.children" must be an array.');
         }
-        if (! is_array($includeTemplates)) {
-            throw new Exception('The option "paperback.include.templates" must be an array.');
+        if (! is_array($excludeTemplate)) {
+            throw new Exception('The option "paperback.exclude.template" must be an array.');
         }
 
         $languages   = site()->languages();
@@ -38,30 +34,21 @@ kirby()->set('route', [
         $version     = site()->version();
         $filename    = str::slug($title);
 
-        if (! $includeInvisibles) {
+		/* Check whether to include invisible pages */
+
+        if (! $includeInvisible) {
             $pages = $pages->visible();
         }
 
-        if ($includePages) {
-            $pages = $pages->find($includePages)->children();
+		/* Include only the children of the specified page */
+
+        if ($includeChildren) {
+            $pages = $pages->find($includeChildren)->children();
         }
 
-/**
-        $pages = $pages
-			->filterBy('intendedTemplate', 'not in', $ignoredTemplates);
-*/
+		/* Exclude pages using certain templates */
 
-        $process = c::get('paperback.process', null);
-
-        if ($process instanceof Closure) {
-            $pages = $process($pages);
-
-            if (! $pages instanceof Collection) {
-                throw new Exception('The option "paperback.process" must return a Collection.');
-            }
-        } elseif (! is_null($process)) {
-            throw new Exception($process . ' is not callable.');
-        }
+        $pages = $pages->filterBy('intendedTemplate', 'not in', $excludeTemplate);
 
         $template = __DIR__ . DS . 'paperback-export.txt.php';
         $paperback  = tpl::load($template, compact('languages', 'pages', 'title', 'description', 'version', 'filename'));
